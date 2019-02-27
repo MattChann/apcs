0e65efb9b49b180b674e554f53f44a25c604c93a
Matt <mchan00@stuy.edu>
2019-02-26 22:01:40 -0500

    added comments to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,11 @@
+/*
+ * A simple class that prints the message "Hello World".
+ * Used to test GitHub, Git, and version control features.
+ */
 public class HelloWorld{
+	/*
+	 * Prints "Hello World" when run.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
