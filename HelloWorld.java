899b771112ac2414289acef2060ccced2d6b2b91
Matt <mchan00@stuy.edu>
2019-02-28 12:57:05 -0500

    fixed JavaDoc commenting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
-/*
+/**
  * A simple class that prints the message "Hello World".
  * Used to test GitHub, Git, and version control features.
  */
 public class HelloWorld{
-	/*
+	/**
 	 * Prints "Hello World" when run.
 	 */
 	public static void main(String[] args){
