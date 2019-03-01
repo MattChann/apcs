99b4faba637d92b0685f5dc80d07e7f22cfcda63
DavidQiuSchoolAccount <dqiu00@stuy.edu>
2019-03-01 13:18:28 -0500

    Added time-aware dog.

--- Retriever.java
+++ Retriever.java
@@ -1,5 +1,12 @@
+import java.time.*;
+
 public class Retriever extends Pet{
     public String speak() {
-        return "arf";
+		int hour = LocalDateTime.now().getHour();
+		if(hour > 9 && hour <= 21) {
+            return "arf"; // Dog is awake between 9 AM and 9 PM.
+		} else {
+            return "zzz"; // Dog is asleep.
+		}
     }
 }
