e8fc269b499ef7e0b45ba3f80a8699b12f0d3594
Matt <mchan00@stuy.edu>
2019-08-04 03:53:52 -0400

    Implemented backtracking

---
+++ MazeSolver.java
@@ -0,0 +1,36 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
+public class MazeSolver {
+    private static Maze maze;
+    private static Maze solvedMaze;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("StringMazes.txt");
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter("\\Z");
+
+        String mazeStr = sc.next();
+        maze = new Maze(mazeStr);
+        System.out.println("Maze to be solved:");
+        System.out.println(maze);
+
+        solveMaze();
+
+        System.out.println("Solved maze:");
+        System.out.println(solvedMaze);
+    }
+
+    public static void solveMaze() {
+        if(maze.accept()) solvedMaze = new Maze(maze);
+        else if(maze.lastIsNoGood());
+        else {
+            for(int[] tile:maze.neighbors(maze.getLastAdded())) {
+                maze.populate(tile);
+                solveMaze();
+                maze.depopulate();
+            }
+        }
+    }
+}
